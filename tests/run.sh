c5187c7f2a512d4f498ae2b802bf0d527320329f
AlanCui4080 <me@alancui.cc>
2023-05-02 13:10:13 +0800

    add: A/B test for testsuite

--- tests/run.sh
+++ tests/run.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 CXX="gcc -xc++"
 
@@ -6,6 +6,7 @@ RED=$(printf "\033[31m")
 GREEN=$(printf "\033[32m")
 YELLOW=$(printf "\033[33m")
 PINK=$(printf "\033[35m")
+CYAN=$(printf "\033[36m")
 NONE=$(printf "\033[39m")
 
 compile() {
@@ -16,17 +17,124 @@ compile() {
 	fi
 
 }
+stdcompile() {
+	if $CXX $1 -o /tmp/nekoxxtest.std.exec -xnone ../src/libnekoxx.a	\
+		-fno-exceptions -fno-rtti
+	then
+		return 1;
+	fi
 
+}
 check_run() {
 	if /tmp/nekoxxtest.exec > /tmp/nekoxxtest.output
 	then
-		echo -n $GREEN"[AC]" $NONE "$1\n"
+		echo -n $GREEN"[AC]" $NONE "$1"
+		echo
+
 	else
-		echo -n $PINK"[RE]" $NONE "$1\n"
+		echo -n $PINK"[RE]" $NONE "$1"
+		echo
+
 		cat /tmp/nekoxxtest.output
 	fi
 }
+stdcheck_run() {
+	if stdcompile $1
+	then
+		echo "${YELLOW}[STD_CE]${NONE} $1"
+		echo
 
+		cat /tmp/nekoxxtest.complier
+		return 1;
+	fi
+
+	if /tmp/nekoxxtest.std.exec > /tmp/nekoxxtest.std.output
+	then
+		std_passed=1
+	else
+		std_passed=0
+	fi
+
+	if /tmp/nekoxxtest.exec > /tmp/nekoxxtest.output
+	then
+		if (($std_passed > 0))
+		then 
+			echo -n $GREEN"[AC]" $NONE "$1"
+			echo
+		else
+			echo -n $CYAN"[INCONSISTENT_AC]" $NONE "$1"
+			echo
+			diff -u  /tmp/nekoxxtest.output /tmp/nekoxxtest.std.output
+		fi
+	else
+		if (($std_passed > 0))
+		then 
+			echo -n $CYAN"[INCONSISTENT_WA]" $NONE "$1"
+			echo
+			diff -u /tmp/nekoxxtest.output /tmp/nekoxxtest.std.output
+		else
+			echo -n $RED"[WA]" $NONE "$1"
+			echo
+			cat /tmp/nekoxxtest.output
+		fi
+	fi
+}
+
+stdrun_and_cmp() {
+	if stdcompile $1
+	then
+		echo "${YELLOW}[STD_CE]${NONE} $1"
+		echo
+
+		cat /tmp/nekoxxtest.complier
+		return 1;
+	fi
+
+	if ! /tmp/nekoxxtest.std.exec > /tmp/nekoxxtest.std.output
+	then
+		echo -n $PINK"[STD_RE]" $NONE "$1\n"
+	else
+		if diff /tmp/nekoxxtest.std.output $1.expect > /tmp/nekoxxtest.std.diff
+		then
+			std_passed=1
+		else
+			std_passed=0
+		fi
+	fi
+
+	if ! /tmp/nekoxxtest.exec > /tmp/nekoxxtest.output
+	then
+		echo -n $PINK"[RE]" $NONE "$1\n"
+	else
+		if diff /tmp/nekoxxtest.output $1.expect > /tmp/nekoxxtest.diff
+		then
+			if diff /tmp/nekoxxtest.diff /tmp/nekoxxtest.std.diff
+			then
+				echo -n $GREEN"[AC]" $NONE "$1"
+				echo
+				
+			else
+				echo -n $CYAN"[INCONSISTENT_AC]" $NONE "$1"
+				echo
+
+				diff -u /tmp/nekoxxtest.diff /tmp/nekoxxtest.std.diff
+			fi
+		else
+			if diff /tmp/nekoxxtest.diff /tmp/nekoxxtest.std.diff
+			then
+				echo -n $RED"[WA]" $NONE "$1"
+				echo
+				cat /tmp/nekoxxtest.diff
+			else
+				
+				echo -n $CYAN"[INCONSISTENT_WA]" $NONE "$1"
+				echo
+
+				diff -u /tmp/nekoxxtest.diff /tmp/nekoxxtest.std.diff
+			fi
+		fi
+	fi
+}
 run_and_cmp() {
 	if ! /tmp/nekoxxtest.exec > /tmp/nekoxxtest.output
 	then
@@ -35,14 +143,17 @@ run_and_cmp() {
 	else
 		if diff /tmp/nekoxxtest.output $1.expect > /tmp/nekoxxtest.diff
 		then
-			echo -n $GREEN"[AC]" $NONE "$1\n"
+			echo -n $GREEN"[AC]" $NONE "$1"
+			echo
+
 		else
-			echo -n $RED"[WA]" $NONE "$1\n"
+			echo -n $RED"[WA]" $NONE "$1"
+			echo
+
 			cat /tmp/nekoxxtest.diff
 		fi
 	fi
 }
-
 run_test() {
 	if compile $1
 	then
@@ -57,6 +168,12 @@ run_test() {
 			;;
 		*.cmp.cpp)
 			run_and_cmp $1;
+			;;
+		*.cmpstd.cpp)
+			stdrun_and_cmp $1;
+			;;
+		*.runstd.cpp)
+			stdcheck_run $1;
 			;;
 		*)
 			;;
