28d8f9bcf13c9638c42a9e0543e11e31d7fd4dd4
AlanCui4080 <me@alancui.cc>
2023-05-09 07:55:35 +0800

    add: -Ox optizime test for testsuite

--- tests/run.sh
+++ tests/run.sh
@@ -10,16 +10,14 @@ CYAN=$(printf "\033[36m")
 NONE=$(printf "\033[39m")
 
 compile() {
-	if $CXX $1 -o /tmp/nekoxxtest.exec -xnone ../src/libnekoxx.a	\
-		-fno-exceptions -fno-rtti -nostdinc++ -I ../include
+	if $CXX $1 $2	 -o /tmp/nekoxxtest.exec -xnone ../src/libnekoxx.a -fno-exceptions -fno-rtti -nostdinc++ -I ../include 
 	then
 		return 1;
 	fi
 
 }
 stdcompile() {
-	if $CXX $1 -o /tmp/nekoxxtest.std.exec -xnone ../src/libnekoxx.a	\
-		-fno-exceptions -fno-rtti
+	if $CXX $1 $2	 -o /tmp/nekoxxtest.std.exec -xnone ../src/libnekoxx.a -fno-exceptions -fno-rtti
 	then
 		return 1;
 	fi
@@ -28,20 +26,20 @@ stdcompile() {
 check_run() {
 	if /tmp/nekoxxtest.exec > /tmp/nekoxxtest.output
 	then
-		echo -n $GREEN"[AC]" $NONE "$1"
+		echo -n $GREEN"[AC] $2	" $NONE "$1"
 		echo
 
 	else
-		echo -n $PINK"[RE]" $NONE "$1"
+		echo -n $PINK"[RE] $2	" $NONE "$1"
 		echo
 
 		cat /tmp/nekoxxtest.output
 	fi
 }
 stdcheck_run() {
-	if stdcompile $1
+	if stdcompile $1 $2	
 	then
-		echo "${YELLOW}[STD_CE]${NONE} $1"
+		echo "${YELLOW}[STD_CE] $2	${NONE} $1"
 		echo
 
 		cat /tmp/nekoxxtest.complier
@@ -59,21 +57,21 @@ stdcheck_run() {
 	then
 		if (($std_passed > 0))
 		then 
-			echo -n $GREEN"[AC]" $NONE "$1"
+			echo -n $GREEN"[AC] $2	" $NONE "$1"
 			echo
 		else
-			echo -n $CYAN"[INCONSISTENT_AC]" $NONE "$1"
+			echo -n $CYAN"[INCONSISTENT_AC] $2	" $NONE "$1"
 			echo
 			diff -u  /tmp/nekoxxtest.output /tmp/nekoxxtest.std.output
 		fi
 	else
 		if (($std_passed > 0))
 		then 
-			echo -n $CYAN"[INCONSISTENT_WA]" $NONE "$1"
+			echo -n $CYAN"[INCONSISTENT_WA] $2	" $NONE "$1"
 			echo
 			diff -u /tmp/nekoxxtest.output /tmp/nekoxxtest.std.output
 		else
-			echo -n $RED"[WA]" $NONE "$1"
+			echo -n $RED"[WA] $2	" $NONE "$1"
 			echo
 			cat /tmp/nekoxxtest.output
 		fi
@@ -81,7 +79,7 @@ stdcheck_run() {
 }
 
 stdrun_and_cmp() {
-	if stdcompile $1
+	if stdcompile $1 $2	
 	then
 		echo "${YELLOW}[STD_CE]${NONE} $1"
 		echo
@@ -92,7 +90,7 @@ stdrun_and_cmp() {
 
 	if ! /tmp/nekoxxtest.std.exec > /tmp/nekoxxtest.std.output
 	then
-		echo -n $PINK"[STD_RE]" $NONE "$1\n"
+		echo -n $PINK"[STD_RE] $2	" $NONE "$1\n"
 	else
 		if diff /tmp/nekoxxtest.std.output $1.expect > /tmp/nekoxxtest.std.diff
 		then
@@ -104,17 +102,17 @@ stdrun_and_cmp() {
 
 	if ! /tmp/nekoxxtest.exec > /tmp/nekoxxtest.output
 	then
-		echo -n $PINK"[RE]" $NONE "$1\n"
+		echo -n $PINK"[RE] $2	" $NONE "$1\n"
 	else
 		if diff /tmp/nekoxxtest.output $1.expect > /tmp/nekoxxtest.diff
 		then
 			if diff /tmp/nekoxxtest.diff /tmp/nekoxxtest.std.diff
 			then
-				echo -n $GREEN"[AC]" $NONE "$1"
+				echo -n $GREEN"[AC] $2	" $NONE "$1"
 				echo
 				
 			else
-				echo -n $CYAN"[INCONSISTENT_AC]" $NONE "$1"
+				echo -n $CYAN"[INCONSISTENT_AC] $2	" $NONE "$1"
 				echo
 
 				diff -u /tmp/nekoxxtest.diff /tmp/nekoxxtest.std.diff
@@ -122,12 +120,12 @@ stdrun_and_cmp() {
 		else
 			if diff /tmp/nekoxxtest.diff /tmp/nekoxxtest.std.diff
 			then
-				echo -n $RED"[WA]" $NONE "$1"
+				echo -n $RED"[WA] $2	" $NONE "$1"
 				echo
 				cat /tmp/nekoxxtest.diff
 			else
 				
-				echo -n $CYAN"[INCONSISTENT_WA]" $NONE "$1"
+				echo -n $CYAN"[INCONSISTENT_WA] $2	" $NONE "$1"
 				echo
 
 				diff -u /tmp/nekoxxtest.diff /tmp/nekoxxtest.std.diff
@@ -138,16 +136,16 @@ stdrun_and_cmp() {
 run_and_cmp() {
 	if ! /tmp/nekoxxtest.exec > /tmp/nekoxxtest.output
 	then
-		echo -n $PINK"[RE]" $NONE "$1\n"
+		echo -n $PINK"[RE] $2	" $NONE "$1\n"
 		ERR=1;
 	else
 		if diff /tmp/nekoxxtest.output $1.expect > /tmp/nekoxxtest.diff
 		then
-			echo -n $GREEN"[AC]" $NONE "$1"
+			echo -n $GREEN"[AC] $2	" $NONE "$1"
 			echo
 
 		else
-			echo -n $RED"[WA]" $NONE "$1"
+			echo -n $RED"[WA] $2	" $NONE "$1"
 			echo
 
 			cat /tmp/nekoxxtest.diff
@@ -155,7 +153,7 @@ run_and_cmp() {
 	fi
 }
 run_test() {
-	if compile $1
+	if compile $1 $2	
 	then
 		echo "${YELLOW}[CE]${NONE} $1"
 		cat /tmp/nekoxxtest.complier
@@ -164,16 +162,40 @@ run_test() {
 
 	case $1 in
 		*.run.cpp)
-			check_run $1;
+			check_run $1 -O0;
+			check_run $1 -O1;
+			check_run $1 -O2;
+			check_run $1 -O3;
+			check_run $1 -Os;
+			check_run $1 -Ofast;
+			check_run $1 -Og;
 			;;
 		*.cmp.cpp)
-			run_and_cmp $1;
+			run_and_cmp $1 -O0;
+			run_and_cmp $1 -O1;
+			run_and_cmp $1 -O2;
+			run_and_cmp $1 -O3;
+			run_and_cmp $1 -Os;
+			run_and_cmp $1 -Ofast;
+			run_and_cmp $1 -Os;
 			;;
 		*.cmpstd.cpp)
-			stdrun_and_cmp $1;
+			stdrun_and_cmp $1 -O0;
+			stdrun_and_cmp $1 -O1;
+			stdrun_and_cmp $1 -O2;
+			stdrun_and_cmp $1 -O3;
+			stdrun_and_cmp $1 -Os;
+			stdrun_and_cmp $1 -Ofast;
+			stdrun_and_cmp $1 -Og;
 			;;
 		*.runstd.cpp)
-			stdcheck_run $1;
+			stdcheck_run $1 -O0;
+			stdcheck_run $1 -O1;
+			stdcheck_run $1 -O2;
+			stdcheck_run $1 -O3;
+			stdcheck_run $1 -Os;
+			stdcheck_run $1 -Ofast;
+			stdcheck_run $1 -Og;
 			;;
 		*)
 			;;
